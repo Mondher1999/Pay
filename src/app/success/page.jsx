e46b6500597ab81e49cada44a864d30553a2ad92
Mondher1999 <132766993+Mondher1999@users.noreply.github.com>
2025-07-18 18:42:01 +0100

    Update page.jsx

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -25,7 +25,7 @@ export default async function Return({ searchParams }) {
     } = session;
 
 
-    const baseURL = env.baseURLAPI;
+    const baseURL = "https://api-xv44jczyxq-uc.a.run.app";
 
 
     const orderPayload = {
